1246814ee552e54adc46263d25fb8947b8cda5e2
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2020-08-22 19:18:14 +0200

    Adding some things to initialized component.

--- frontend-svelte/src/components/Initialized.svelte
+++ frontend-svelte/src/components/Initialized.svelte
@@ -1,1 +1,25 @@
-<p>Initialized</p>
+<script>
+  import Error from "./_error.svelte";
+  import LoginForm from "./LoginForm.svelte";
+
+  async function whoami() {
+    let res = await fetch("/users/whoami");
+    if (!res.ok) {
+      throw new Error("Not logged in");
+    }
+    return res;
+  }
+  let promise = whoami();
+</script>
+
+<svelte:head>
+  <title>Bella Ciao</title>
+</svelte:head>
+
+{#await promise}
+  <p>...waiting</p>
+{:then user}
+  <p>Im am {user}</p>
+{:catch _}
+  <LoginForm />
+{/await}
