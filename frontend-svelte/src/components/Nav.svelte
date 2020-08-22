2cff002722661f7098dd2edf43c83b7db748f538
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2020-08-22 18:40:23 +0200

    Removed default Sapper stuff and added bootstrap.

--- frontend-svelte/src/components/Nav.svelte
+++ frontend-svelte/src/components/Nav.svelte
@@ -1,60 +1,28 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+nav.navbar {
+	margin-bottom: 10px;
+}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
+  <a class="navbar-brand" href=".">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="{segment === 'faq' ? 'nav-item active' : 'nav-item'}">
+        <a class="nav-link" aria-current={segment === 'faq' ? 'page' : undefined} href="faq">
+		  faq
+		  {#if segment === 'faq'}
+		  <span class="sr-only">(current)</span>
+		  {/if}
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
---
+++ frontend-svelte/src/routes/faq.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>FAQ</title>
+</svelte:head>
+
+<h1>Frequently asked questions</h1>
